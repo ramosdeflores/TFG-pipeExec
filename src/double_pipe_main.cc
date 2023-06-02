b7bcee6dd7c3a949cc01dbe04f03337211f3b54e
lucashdez <lukastpiano@gmail.com>
2023-06-02 21:45:55 +0100

    A little step on the double pipe

--- src/double_pipe_main.cc
+++ src/double_pipe_main.cc
@@ -15,39 +15,37 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * Author:  Lucas Hernández Abreu
- * Contact: lucas.hernandez.09@ull.edu.es
+ * Author:  Lucas Hernández Abreu Contact: lucas.hernandez.09@ull.edu.es
  */
 
 #include "processing_units.h"
 
 int
 DoublePipeMain(bool debug_flag, bool pu_debug_flag) {
-    DoublePipe double_pipe;
-    MemoryManager *data_in = new MemoryManager(5, debug_flag);
-    for (size_t it = 0; it < 5; ++it) {
-        int* holder = new int(0);
-        data_in->LoadMemoryManager(holder);
+  DoublePipe double_pipe;
+  MemoryManager *data_in = new MemoryManager(5, debug_flag);
+  for (size_t it = 0; it < 5; ++it) {
+    int *holder = new int(0);
+    data_in->LoadMemoryManager(holder);
+  }
+  Pipeline *pipe = new Pipeline(&double_pipe, data_in, 1, debug_flag);
+  pipe->RunPipe();
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 5; ++i) {
+    if (debug_flag) {
+      printf("%s(main) Popping from IN %s\n", LUCID_CYAN, LUCID_NORMAL);
     }
-    Pipeline *pipe = new Pipeline(&double_pipe, data_in, 1, debug_flag);
-    pipe->RunPipe();
-    
-    
-    auto t1 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 5; ++i) {
-        if (debug_flag) {
-            printf("%s(main) Popping from IN %s\n", LUCID_CYAN, LUCID_NORMAL);
-        }
-        int *data_handler = (int *)data_in->PopFromIn();
-        if (debug_flag) {
-            printf("%s(main) Pushing into OUT %s\n", LUCID_CYAN, LUCID_NORMAL);
-        }
-        data_in->PushIntoOut(data_handler);
+    int *data_handler = (int *)data_in->PopFromIn();
+    if (debug_flag) {
+      printf("%s(main) Pushing into OUT %s\n", LUCID_CYAN, LUCID_NORMAL);
     }
-    pipe->WaitFinish();
-    auto t2 = std::chrono::high_resolution_clock::now();
-    
-    printf("Time Elapsed running the pipe: %fs\n",
-           std::chrono::duration<double>(t2 - t1).count());
-    return 0;
+    data_in->PushIntoOut(data_handler);
+  }
+  pipe->WaitFinish();
+  auto t2 = std::chrono::high_resolution_clock::now();
+
+  printf("Time Elapsed running the pipe: %fs\n",
+         std::chrono::duration<double>(t2 - t1).count());
+  return 0;
 }
